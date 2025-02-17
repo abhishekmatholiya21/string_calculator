ff2632eeb9c2c1e2dcd095e53931b19893b473ac
abhishekmatholiya <abhishekmatholiya21@gmail.com>
2025-02-18 02:01:49 +0530

    Implement functionality to handle 2 number input

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 # StringCalculator class acts as a calculator for adding numbers provided in a string format.
 class StringCalculator
   def self.add(numbers)
-    numbers.empty? ? 0 : numbers.to_i
+    return 0 if numbers.empty?
+    numbers.split(',').map(&:to_i).sum
   end
 end
