e5a7ff9d859beaadf58e574283d4070dad854e0d
abhishekmatholiya <abhishekmatholiya21@gmail.com>
2025-02-18 01:09:39 +0530

    Add initial tests for StringCalculator following TDD principles

---
+++ README.md
@@ -0,0 +1,30 @@
+# String Calculator
+
+A simple Ruby library that performs basic string-based calculations.
+
+## Features
+
+- Add numbers in a string.
+- Handle custom delimiters.
+- Raise exceptions for invalid input, such as negative numbers.
+
+## System Requirements
+
+Before running this project, ensure your system meets the following requirements:
+
+- **Ruby**: Version 2.7 or higher.
+- **Bundler**: Install Bundler (`gem install bundler`).
+- **Operating System**: Compatible with macOS, Linux, or Windows.
+
+## Installation
+
+1. Clone the repository Run `git clone https://github.com/abhishekmatholiya21/string_calculator_tdd.git`.
+2. Run `bundle install` to install dependencies.
+3. Run `rspec` to run the tests.
+
+## Screenshots
+- Failing Rspec
+![alt text](image.png)
+
+- Passing Rspec
+![alt text](image-1.png)
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,41 @@
+require 'string_calculator'
+
+describe StringCalculator do
+  describe "#add" do
+    context "when an empty string is passed" do
+      it "returns 0" do
+        expect(described_class.add("")).to eq(0)
+      end
+    end
+
+    context "when a single number is passed" do
+      it "returns 4" do
+        expect(described_class.add("4")).to eq(4)
+      end
+    end
+
+    context "when two numbers '2,4' are passed" do
+      it "returns 6" do
+        expect(described_class.add("2,4")).to eq(6)
+      end
+    end
+
+    context "when a new line and numbers '1\\n2,3' are passed" do
+      it "returns 6" do
+        expect(described_class.add("1\n2,3")).to eq(6)
+      end
+    end
+    
+    context "when a custom delimiter '//;\\n1;2' is passed" do
+      it "returns 3" do
+        expect(described_class.add("//;\n1;2")).to eq(3)
+      end
+    end
+
+    context "when negative numbers '-1, 22, -2, -3, 5' are passed" do
+      it "throws an exception" do
+        expect { described_class.add("-1, 22,-2,-3, 5") }.to raise_error("negative numbers not allowed -1, -2, -3")
+      end
+    end
+  end
+end
