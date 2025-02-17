c48ed98b21f0b03017468d226e50ae98855bdd2d
abhishekmatholiya <abhishekmatholiya21@gmail.com>
2025-02-18 01:23:03 +0530

    Implement handling of empty string, return 0

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 # StringCalculator class acts as a calculator for adding numbers provided in a string format.
 class StringCalculator
   def self.add(numbers)
+    return 0 if numbers.empty?
   end
 end
